1da90f42cb6034d1a684b57eaeb9856613b64045
Aaron Brooks <aaron@brooks1.net>
2010-01-27 01:25:18 -0500

    It's alive!!! Clojure port of http://xcb.freedesktop.org/tutorial/ works.
    I need to remove lots of diagnostics but I'm checking it in while it's
    still working in case clean-up breaks something. Yes, it's been one of
    those evenings. These chunks are twisty and gross with no
    documentation.

--- src/prion.clj
+++ src/prion.clj
@@ -1,16 +1,70 @@
 (ns prion
     (:gen-class)
-    (:use [clj-native.core :only [defclib]]))
+    (:use [clj-native.core :only [defclib]])
+    (:import [java.nio ByteBuffer ByteOrder]
+             [com.sun.jna.ptr IntByReference]))
 
-(System/setProperty "jna.library.path" "/usr/lib/libc.so")
+(System/setProperty "jna.library.path" "/usr/lib/libxcb.so")
 
 (defclib
-  c
+  xcb
+  (:structs
+    (xcb_screen_t
+      :root i32
+      :default_colormap i32
+      :white_pixel i32
+      :black_pixel i32
+      :current_input_masks i32
+      :width_in_pixels i16
+      :height_in_pixels i16
+      :width_in_millimeters i16
+      :height_in_millimeters i16
+      :min_installed_maps i16
+      :max_installed_maps i16
+      :root_visual i32
+      :backing_stores i8
+      :save_unders i8
+      :root_depth i8
+      :allowed_depths_len i8)
+    (xcb_screen_iterator_t :data xcb_screen_t* :rem int :index int))
   (:functions
-    (sleep [ int ] void)))
+    (xcb_connect [ constchar* void* ] void*)
+    (xcb_get_setup [ void* ] void*)
+    (xcb_setup_roots_iterator [ void* ] xcb_screen_iterator_t)
+    (xcb_screen_next [ xcb_screen_iterator_t* ] void )))
 
-(loadlib-c)
+(loadlib-xcb)
+
+(defn direct-int-buffer [size]
+      (let [byte-buffer (ByteBuffer/allocateDirect (* size 4))]
+        (.order byte-buffer (ByteOrder/nativeOrder))
+        (.asIntBuffer byte-buffer)))
 
 (defn -main [& args]
-      (println "prion args" args)
-      (time (sleep (Integer. (nth args 0)))))
+      (let [screenNum (new IntByReference 7)
+            _ (println "screenNum value" (.getValue screenNum))
+            screenNum# (.getPointer screenNum)
+            _ (println "screenNum" screenNum#)
+            conn (xcb_connect nil screenNum#)
+            _ (println "conn" conn)
+            _ (println "screenNum value" (.getValue screenNum))
+            setup (xcb_get_setup conn)
+            _ (println "setup" setup)
+            iter (xcb_setup_roots_iterator setup)
+            _ (println "iter" iter)
+            screen (do
+                    (dotimes [_ (.getValue screenNum)]
+                        (xcb_screen_next iter))
+                    (println "final iter" iter)
+                    (println "final bean" (bean iter))
+                    (println "final :data" (.data iter))
+                    (println "done")
+                    (.data iter))]
+        (println)
+        (println "xcb_screen_t" screen)
+        (println "Information of screen" (.root screen))
+        (println "  width.........:" (.width_in_pixels screen))
+        (println "  height........:" (.height_in_pixels screen))
+        (println "  white pixel...:" (.white_pixel screen))
+        (println "  black pixel...:" (.black_pixel screen))
+        (println)))
