f85c0aea878896060c7900ec0ea346853d94d367
Aaron Brooks <aaron@brooks1.net>
2010-03-22 21:30:46 -0400

    Rework XCB demo to break out connection and grab managers.

--- src/prion.clj
+++ src/prion.clj
@@ -7,10 +7,12 @@
     (:import [java.nio ByteBuffer ByteOrder]))
 
 (System/setProperty "jna.library.path" "/usr/lib/libxcb.so")
+;(System/setProperty "jna.library.path" "/usr/lib/debug/usr/lib/libxcb.so.1.1.0")
 
 (defclib
   xcb
   (:structs
+    (xcb_connection_t #__opaque)
     (xcb_screen_t
       :root i32
       :default_colormap i32
@@ -31,30 +33,66 @@
     (xcb_screen_iterator_t :data xcb_screen_t* :rem int :index int))
   (:functions
     (xcb_connect [ constchar* int* ] void*)
+    (xcb_disconnect [ void* ] void )
+    (xcb_connection_has_error [ void* ] int)
+    (xcb_generate_id [ void* ] i32)
+    (xcb_grab_server [ void* ] void )
+    (xcb_ungrab_server [ void* ] void )
     (xcb_get_setup [ void* ] void*)
     (xcb_setup_roots_iterator [ void* ] xcb_screen_iterator_t)
     (xcb_screen_next [ xcb_screen_iterator_t* ] void )))
 
-(loadlib-xcb)
 
-(defn direct-int-buffer [size]
-      (let [byte-buffer (ByteBuffer/allocateDirect (* size 4))]
-        (.order byte-buffer (ByteOrder/nativeOrder))
-        (.asIntBuffer byte-buffer)))
+(def *xconn* (atom nil))
+(def *xdflscr* (atom nil))
+
+(defn cInt
+  ([]
+   (cInt 1))
+  ([size]
+   (let [byte-buffer (ByteBuffer/allocateDirect (* size 4))]
+     (.order byte-buffer (ByteOrder/nativeOrder))
+     (.asIntBuffer byte-buffer))))
+
+(defmacro with-grab [ body ]
+  `(do
+    (xcb_grab_server @*xconn*)
+    ~body
+    (xcb_ungrab_server @*xconn*)))
+
+(defn x-connection []
+  (let [scrn (cInt)
+              conn (xcb_connect nil scrn)
+              xerr (xcb_connection_has_error conn)]
+    (if (not= 0 xerr)
+      (throw (Exception. (pr-str "Error opening display:" xerr))))
+    (reset! *xconn* conn)
+    (reset! *xdflscr* (.get scrn 0))))
+
+(defmacro with-connection [ body ]
+  `(do
+     (x-connection)
+     ~body
+     (xcb_disconnect @*xconn*)))
+
+(defn get_setup []
+  (xcb_get_setup @*xconn*))
 
 (defn -main [& args]
-      (let [scr-num (direct-int-buffer 1)
-            conn (xcb_connect nil scr-num)
-            setup (xcb_get_setup conn)
-            iter (xcb_setup_roots_iterator setup)
-            screen (do
-                    (dotimes [_ (.get scr-num)]
-                        (xcb_screen_next iter))
-                    (.data iter))]
-        (println)
-        (println "Information of screen" (.root screen))
-        (println "  width.........:" (.width_in_pixels screen))
-        (println "  height........:" (.height_in_pixels screen))
-        (println "  white pixel...:" (.white_pixel screen))
-        (println "  black pixel...:" (.black_pixel screen))
-        (println)))
+  (do
+    (loadlib-xcb)
+    (with-connection
+      (with-grab
+        (let [iter (xcb_setup_roots_iterator (get_setup))
+              screen (do
+                        (dotimes [_ 0 #_(@xdflscr)]
+                          (xcb_screen_next iter))
+                        (.data iter))]
+          (println)
+          (println "Information of screen" (.root screen))
+          (println "  width.........:" (.width_in_pixels screen))
+          (println "  height........:" (.height_in_pixels screen))
+          (println "  white pixel...:" (.white_pixel screen))
+          (println "  black pixel...:" (.black_pixel screen))
+          (println))))))
+
